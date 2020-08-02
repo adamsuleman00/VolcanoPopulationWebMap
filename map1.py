db6a75c49908edf148f0434d9026e438f567babf
Adam Suleman <43348989+adamsuleman00@users.noreply.github.com>
2020-08-02 20:57:55 +0100

    Update map1.py

--- map1.py
+++ map1.py
@@ -6,14 +6,30 @@ lat = list(data['LAT'])
 lon = list(data['LON'])
 elev = list(data['ELEV'])
 
-map = folium.Map(location = [38.58,-99.09], zoom_start = 6, tiles = "Stamen Terrain")  
+html = """<h4>Volcano information:</h4>
+Height: %s m
+"""
 
-fg = folium.FeatureGroup(name="My Map") 
+def color_producer(elevation):
+    if elevation < 1000:
+        return '#00cc00'
+    elif 1000 <= elevation <3000:
+        return '#ff9900'
+    else:
+        return '#ff0000'
 
+map = folium.Map(location=[38.58, -99.09], zoom_start=5, tiles="stamentoner")
+fg = folium.FeatureGroup(name = "My Map")
+ 
 for lt, ln, el in zip(lat, lon, elev):
-    fg.add_child(folium.Marker(location = [lt, ln], popup=str(el) +" m", icon = folium.Icon(color='red')))
-
-
+    iframe = folium.IFrame(html=html % str(el), width=200, height=100)
+    fg.add_child(folium.CircleMarker(location=[lt, ln], radius=10, popup=folium.Popup(iframe), 
+    fill_color=color_producer(el), color = '#787878'))
+ 
+fg.add_child(folium.GeoJson(data=open('world.json', 'r', encoding='utf-8-sig').read(), 
+style_function=lambda x: {'fillColor':'green' if x['properties']['POP2005'] < 10000000
+else 'orange' if 10000000 <= x['properties']['POP2005'] < 20000000 else 'red'}))
+ 
 map.add_child(fg) 
 
 map.save("Map1.html")         
